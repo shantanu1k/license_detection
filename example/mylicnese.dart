0f94fe3494e319f2686347be2ed7f8d63a7c886c
Shantanu Kawade <shantanukawade1@gmail.com>
2021-03-15 10:49:00 +0530

    Added comments

--- example/mylicnese.dart
+++ example/mylicnese.dart
@@ -2,7 +2,7 @@ import 'package:license_detection/License.dart';
 import 'package:license_detection/license_detection.dart';
 class MyLicense{
   //This is MIT License
-  //My modification on line 20, col 5
+  //My modification on line 21, col 5
   String mylicense = '''
     Copyright (c) 2021 Shantanu Kawade.
     Permission is hereby granted, free of charge, to any person obtaining a copy
